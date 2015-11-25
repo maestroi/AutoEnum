70ff976c156615db867bd51f152d124b038d11ce
Maestroi <chuck@tonec.nl>
2015-11-26 00:56:45 +0100

    v2 test version update tool added

--- v2/main.py
+++ v2/main.py
@@ -131,12 +131,14 @@ while loop:          ## While loop which will keep going until loop = False
             if choice==1:
                 cls()
                 print "je hebt gekozen voor stable"
+                os.system("mv main.py main-old.py")
                 os.system("wget https://github.com/maestroi/autoenum/blob/master/main.py")
                 time.sleep(3)
                 os.execl(sys.executable, sys.executable, *sys.argv)
             elif choice==2:
                 cls()
                 print "je hebt gekozen voor beta"
+                os.system("mv main.py main-old.py")
                 os.system("wget https://raw.githubusercontent.com/maestroi/autoenum/dubbel/v2/main.py")
                 time.sleep(3)
                 os.execl(sys.executable, sys.executable, *sys.argv)
